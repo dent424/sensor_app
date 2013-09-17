aba731d2dba10b27eaf2a481b69e11794be7c217
Alex Moore <alexandermoore@gmail.com>
2013-09-17 12:56:12 +0900

    first commit

---
+++ sensor_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SensorApp::Application.config.secret_token = '4359e52b4d62a222bea06ed43d8c4cf26c9870cdf58ce76792e6e13c64aa532f4f7e78083ad5a658186c7e57623232cb45ce1a373c1239d43538547bcad39fbf'
